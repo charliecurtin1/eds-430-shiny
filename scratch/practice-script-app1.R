69d3cbbe5b7b47e73334a4427f9269377e5fa22b
charliecurtin1 <charliecurtin1@gmail.com>
2024-01-26 12:08:15 -0800

    finished part 1

---
+++ scratch/practice-script-app1.R
@@ -0,0 +1,32 @@
+# load packages -----
+library(palmerpenguins)
+library(tidyverse)
+
+# filter penguins df for obs where body mass is >= 3000 and <= 4000
+body_mass_df <- penguins %>% 
+  filter(body_mass_g %in% c(3000:4000))
+
+# create scatterplot -----
+ggplot(na.omit(body_mass_df),
+       aes(x = flipper_length_mm, 
+           y = bill_length_mm,
+           color = species,
+           shape = species)) +
+  geom_point() +
+  scale_color_manual(values = c("darkorange", "purple", "cyan4")) +
+  scale_shape_manual(values = c(19, 17, 15)) +
+  labs(x = "Flipper length (mm)", 
+       y = "Bill length (mm)",
+       color = "Penguin species",
+       shape = "Penguin species") +
+  theme_minimal() +
+  theme(legend.position = c(.85, .2),
+        legend.background = element_rect(color = "white"))
+
+
+# filter penguins df for year
+penguins_year_df <- penguins %>% 
+  filter(year %in% c(2007:2008))
+
+# show datatable for penguins
+DT::datatable(penguins_year_df)
---
+++ single-file-app/app.R
@@ -0,0 +1,85 @@
+# load packages ----- 
+library(shiny)
+library(palmerpenguins)
+library(tidyverse)
+library(DT)
+
+# user interface -----
+ui <- fluidPage(
+  
+  # app title -----
+  tags$h1("Charlie's app"),
+  
+  # app subtitle -----
+  h4(strong("exploring little penguins stuff")),
+  
+  # body mass slider input -----
+  sliderInput(inputId = "body_mass_input",
+              label = "Select a range of body masses (g):",
+              min = 2700, max = 6300, value = c(3000, 4000)),
+  
+  # body mass plot output -----
+  plotOutput(outputId = "body_mass_scatterplot_output"),
+  
+  # year input -----
+  checkboxGroupInput(inputId = "year_input",
+                     label = "Select year(s)",
+                     choices = unique(penguins$year),
+                     selected = c("2007", "2008")),
+  
+  # DT datable output -----
+  DT::dataTableOutput(outputId = "penguins_DT_output")
+  
+)
+
+# server -----
+server <- function(input, output) {
+  
+  # render penguin scatterplot -----
+  output$body_mass_scatterplot_output <- renderPlot({
+    
+    # filter body masses -----
+    body_mass_df <- reactive({
+      
+      penguins %>% 
+        filter(body_mass_g %in% c(input$body_mass_input[1]:input$body_mass_input[2]))
+      
+    })
+    
+    # plot penguins scatterplot
+    ggplot(na.omit(body_mass_df()),
+           aes(x = flipper_length_mm, 
+               y = bill_length_mm,
+               color = species,
+               shape = species)) +
+      geom_point() +
+      scale_color_manual(values = c("Adelie" = "darkorange", "Chinstrap" = "purple", "Gentoo" = "cyan4")) +
+      scale_shape_manual(values = c("Adelie" = 19, "Chinstrap" = 17, "Gentoo" = 15)) +
+      labs(x = "Flipper length (mm)", 
+           y = "Bill length (mm)",
+           color = "Penguin species",
+           shape = "Penguin species") +
+      theme_minimal() +
+      theme(legend.position = c(.85, .2),
+            legend.background = element_rect(color = "white"))
+    
+  })
+  
+  # filter for penguins year -----
+  penguins_year_df <- reactive({
+    
+    penguins %>% 
+      filter(year %in% c(input$year_input))
+    
+  })
+  
+  # penguins data table year checkbox -----
+  output$penguins_DT_output <- DT::renderDataTable({
+    
+    # create datatable for the year
+    DT::datatable(penguins_year_df())
+  })
+}
+
+# combine ui and server into app -----
+shinyApp(ui = ui, server = server)
